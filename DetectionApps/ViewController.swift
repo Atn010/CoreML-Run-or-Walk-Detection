612d4f2a81304e6c25be847eff8845e716f0a8a6
Handy <handy.kwok@gmail.com>
2018-11-22 10:55:06 +0700

    Initial Commit

---
+++ DetectionApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectionApps
+//
+//  Created by Handy Handy on 22/11/18.
+//  Copyright © 2018 Handy Handy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
