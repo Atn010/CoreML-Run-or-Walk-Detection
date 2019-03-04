7548834fd2f06514f8cc19a03f6227ffea0614fa
Atn010 <>
2019-03-04 15:14:26 +0700

    Added Run and Walk image

---
+++ DetectionApps/Image+Resize.swift
@@ -0,0 +1,99 @@
+//
+//  Image+Resize.swift
+//  DetectionApps
+//
+//  Created by Handy Handy on 22/11/18.
+//  Copyright © 2018 Handy Handy. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+// MARK: - Extension resize the image based on what we need
+extension UIImage {
+    func resizeTo(size: CGFloat) -> UIImage? {
+        // cgImage is an attribute of UIImage
+        guard let cgimage = self.cgImage else {return nil}
+        let contextImage: UIImage = UIImage(cgImage: cgimage)
+        let contextSize: CGSize = contextImage.size
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(size)
+        var cgheight: CGFloat = CGFloat(size)
+        
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+        
+        // Create bitmap image from context using the rect
+        guard let imageRef: CGImage = cgimage.cropping(to: rect) else {return nil}
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(cgImage: imageRef, scale: self.scale, orientation: self.imageOrientation)
+        
+        let newScale = size / image.size.width
+        
+        let newWidth = image.size.width * newScale
+        let newHeight = image.size.height * newScale
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    func pixelBuffer() -> CVPixelBuffer? {
+        let width = self.size.width
+        let height = self.size.height
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                     kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                         Int(width),
+                                         Int(height),
+                                         kCVPixelFormatType_32ARGB,
+                                         attrs,
+                                         &pixelBuffer)
+        
+        guard let resultPixelBuffer = pixelBuffer, status == kCVReturnSuccess else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(resultPixelBuffer)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData,
+                                      width: Int(width),
+                                      height: Int(height),
+                                      bitsPerComponent: 8,
+                                      bytesPerRow: CVPixelBufferGetBytesPerRow(resultPixelBuffer),
+                                      space: rgbColorSpace,
+                                      bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+                                        return nil
+        }
+        
+        context.translateBy(x: 0, y: height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return resultPixelBuffer
+    }
+}
+
--- DetectionApps/ViewController.swift
+++ DetectionApps/ViewController.swift
@@ -7,14 +7,102 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreML
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+	
+	let model = RunWalk()
+	
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    
+    @IBAction func searchDidTap(_ sender: UIButton) {
+        // object responsible to handle photo library / camera access
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        
+        // alert controller > element to present message to the user
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // add button to the alert
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+            // define the source for image picker
+            imagePicker.sourceType = .photoLibrary
+            // present the image picker
+            self.present(imagePicker, animated: true, completion: nil)
+        }))
+		alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+			// define the source for image picker
+			imagePicker.sourceType = .camera
+			// present the image picker
+			self.present(imagePicker, animated: true, completion: nil)
+		}))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
+            (action) in
+            print("cancel")
+        }))
+        
+        
+        // function to present viewControllers
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func updateResult(currentImage: UIImage) {
+        // function for update the result of image and calculate the probability
+		do{
+			guard let image = currentImage.pixelBuffer() else {return}
+			guard let item = try? model.prediction(image: image) else{return}
+			
+			print(item.classLabel)
+			print(item.classLabelProbs)
+			
+			guard let probability = item.classLabelProbs["\(item.classLabel)"] else{return}
+			resultLabel.text = "\(item.classLabel) ~ \(round(probability * 100))%"
+		}catch{
+			print("Error \(error.localizedDescription)")
+		}
+    }
+    
 
 }
+
+
+// MARK: - UIImagePickerControllerDelegate
+// the image picker uses a navigation controller structure, so we need to include the navigation delegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // function triggered when the user cancel the photo selection
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        
+        // after cancel dismiss the picker screen
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // function triggered when the user selects a photo
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // get the selected image
+        // the info parameter has a lot of information about the image
+        // the key to access the original image is UIImagePickerControllerOriginalImage
+        guard let photo = info[.originalImage] as? UIImage else {return}
+        
+        // present the selected image in imageView
+        guard let image = photo.resizeTo(size: 224.0) else {return}
+        imageView.image = image
+        updateResult(currentImage: image)
+        // after getting the image dismiss the picker screen
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
 
